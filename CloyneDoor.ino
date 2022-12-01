d4cafb755ec19904710e99c0f31933efd91aa53f
Abhi Alderman <60452804+AbhiAlderman@users.noreply.github.com>
2022-11-30 23:38:37 -0800

    Add files via upload

--- CloyneDoor.ino
+++ CloyneDoor.ino
@@ -1,6 +1,8 @@
 
 const int relay = 2;
 String receive;
+unsigned long valid[] = {532770, 591974, 531303, 575760};
+int max = sizeof(valid) / sizeof(unsigned long);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,16 +10,37 @@ void setup() {
   digitalWrite(relay, LOW);
 }
 
+
+
+bool validateInput(String hex) {
+  char * pEnd;
+  unsigned long num = strtol (hex.substring(6, 12).c_str(), &pEnd, 16);
+  Serial.println(num);
+  unsigned long mask = 0b000111111111111111111110;
+  num = (num & mask) >> 1;
+  Serial.println(num);
+  for (int i = 0; i < max; i++) {
+    if (valid[i] == num) {
+      Serial.println("Valid code!");
+      return true;
+    }
+  }
+  Serial.println("Not valid code");
+  return false;
+
+  
+}
 void loop() {
   
   if(Serial.available() > 0) {
     receive = Serial.readString();
     Serial.println(receive);
-    /*
-    digitalWrite(tx, HIGH);
+    if (validateInput(receive)) {
+      digitalWrite(relay, HIGH);
+      delay(100);
+      digitalWrite(relay, LOW);
+    }
     delay(100);
-    digitalWrite(tx, LOW);
-    */
   }
   
   delay(100);
